9a9fa96ec60e9ed708b068d3293ce29a462ee878
saarthibhatia <saarthibhatia2010@gmail.com>
2017-07-12 05:06:42 -0700

    Which part of the US has the most Data Engineer jobs in 2013 year

---
+++ h1b2a2013.sql
@@ -0,0 +1,4 @@
+use h1b;
+select split(worksite,'[,]')[1] as state, count(split(worksite,'[,]')[1]) as job_cnt from h1b_final where year = '2013' and job_title LIKE '%DATA ENGINEER%' group by split(worksite,'[,]')[1] order by job_cnt desc limit 1;
+
+
