06b8449a85abef20ef11fa39a47a5c2c051d7d92
saarthibhatia <saarthibhatia2010@gmail.com>
2017-07-12 05:13:03 -0700

    find top 5 locations in the US who have got certified visa in 2014

---
+++ h1b2b2014.sql
@@ -0,0 +1,2 @@
+use h1b;
+select split(worksite,'[,]')[1] as state, count(split(worksite,'[,]')[1]) as job_cnt from h1b_final where year = '2014' and case_status = 'CERTIFIED' group by split(worksite,'[,]')[1] order by job_cnt desc limit 5;
