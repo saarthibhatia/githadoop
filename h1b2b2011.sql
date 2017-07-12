4c6caafcbbc242dbcf7e16395c8989a09b964bbb
saarthibhatia <saarthibhatia2010@gmail.com>
2017-07-12 05:11:56 -0700

    find top 5 locations in the US who have got certified visa in 2011

---
+++ h1b2b2011.sql
@@ -0,0 +1,3 @@
+use h1b;
+select split(worksite,'[,]')[1] as state, count(split(worksite,'[,]')[1]) as job_cnt from h1b_final where year = '2011' and case_status = 'CERTIFIED' group by split(worksite,'[,]')[1] order by job_cnt desc limit 5;
+
